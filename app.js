b179af9d104776455f60d73f7205892a8d5ebdec
doctafaustus <clowd150@gmail.com>
2017-01-22 21:26:30 -0600

    Begin blog

--- app.js
+++ app.js
@@ -102,8 +102,17 @@ app.get('/', function(req, res) {
 });
 
 app.get('/account', function(req, res) {
-	res.render('account.ejs', {recipes: recipes});
+	res.render('account.ejs');
 });
+
+app.get('/blog', function(req, res) {
+  res.render('blog.ejs');
+});
+
+app.get('/blog/its-here-recipe-saver-2-debuts', function(req, res) {
+  res.render('its-here-recipe-saver-2-debuts.ejs');
+});
+
 
 app.get('/recipes', function(req, res) {
   res.render('recipes.ejs', {recipes: recipes});
@@ -452,7 +461,7 @@ var recipes = [
     "url": "http://allrecipes.com/recipe/45641/roasted-rack-of-lamb/?internalSource=hub%20recipe&referringContentType=search%20results&clickId=cardslot%202",
     "ingredients": ["1/2 cup fresh bread crumbs", "2 tablespoons minced garlic", "2 tablespoons chopped fresh rosemary", "1 teaspoon salt", "1/4 teaspoon black pepper", "2 tablespoons olive oil", "1 (7 bone) rack of lamb, trimmed and frenched", "1 teaspoon salt", "1 teaspoon black pepper", "2 tablespoons olive oil"],
     "more": "Preheat oven to 450 degrees F (230 degrees C). Move oven rack to the center position. In a large bowl, combine bread crumbs, garlic, rosemary, 1 teaspoon salt and 1/4 teaspoon pepper. Toss in 2 tablespoons olive oil to moisten mixture. Set aside. Season the rack all over with salt and pepper. Heat 2 tablespoons olive oil in a large heavy oven proof skillet over high heat. Sear rack of lamb for 1 to 2 minutes on all sides. Set aside for a few minutes. Brush rack of lamb with the mustard. Roll in the bread crumb mixture until evenly coated. Cover the ends of the bones with foil to prevent charring.Arrange the rack bone side down in the skillet. Roast the lamb in preheated oven for 12 to 18 minutes, depending on the degree of doneness you want. With a meat thermometer, take a reading in the center of the meat after 10 to 12 minutes and remove the meat, or let it cook longer, to your taste. Let it rest for 5 to 7 minutes, loosely covered, before carving between the ribs.",
-    "tags": [{"name": "dinner", "color": "#00c5ff"}, {"name": "holiday", "color": "#ff05d6"}],
+    "tags": [{"name": "dinner", "color": "#2c77ff"}, {"name": "holiday", "color": "#ff05d6"}],
     date: '1/17/17',
     readyIn: 90,
     cals: '2000',
