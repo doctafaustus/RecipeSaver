20f3205640423a1de38043f9843f90b148363f73
doctafaustus <clowd150@gmail.com>
2017-02-11 01:00:08 -0600

    Add Twitter and Facebook Passport strategies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var passport = require('passport');
-var Strategy = require('passport-twitter').Strategy;
+var TwitterStrategy = require('passport-twitter').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var bodyParser = require('body-parser');
@@ -9,19 +10,37 @@ var fs = require('fs');
 // CUSTOM MODULES
 var emailSupport = require('./mods/emailSupport.js');
 
+
+
 //var Unitz = require('unitz');
 //console.log(Unitz.parse('19/50 tbsps').convert('tsp', true))
 
-
-passport.use(new Strategy({
-    consumerKey: 'sujQmls5lvQVViAog5HTmNr6Z',
-    consumerSecret: 'jrUKqLcG08JyRRHjlfrsnrSmy8NIDH9CSMkTzYKXDSiClOjaFa',
-    callbackURL: 'http://127.0.0.1:3000/login/twitter/return'
+var twitterAppSecret = process.env.PORT ? null : fs.readFileSync('./private/twitterAppSecret.txt').toString();
+passport.use(new TwitterStrategy({
+    consumerKey: 'YjrR2EFz03kHgnTElEsKB6jcC',
+    consumerSecret: twitterAppSecret,
+    callbackURL: process.env.PORT ? null : 'http://127.0.0.1:3000/login/twitter/return'
   },
   function(token, tokenSecret, profile, cb) {
-
     return cb(null, profile);
   }));
+
+var facebookAppSecret = process.env.PORT ? null : fs.readFileSync('./private/facebookAppSecret.txt').toString();
+passport.use(new FacebookStrategy({
+    clientID: '264292990672562',
+    clientSecret: facebookAppSecret,
+    callbackURL: process.env.PORT ? null : 'http://127.0.0.1:3000/login/facebook/callback'
+  },
+  function(token, tokenSecret, profile, cb) {
+    return cb(null, profile);
+  }));
+//   function(accessToken, refreshToken, profile, cb) {
+//     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+//       return cb(err, user);
+//     });
+//   }
+// ));
+
 
 passport.serializeUser(function(user, cb) {
   cb(null, user);
@@ -34,29 +53,23 @@ passport.deserializeUser(function(obj, cb) {
 
 // Create a new Express application.
 var app = express();
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-// Configure view engine to render EJS templates.
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(session({ secret: 'anything' }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
-// app.use(require('cookie-parser')());
-// app.use(require('body-parser').urlencoded({ extended: true }));
-//app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-
-// // Initialize Passport and restore authentication state, if any, from the
-// // session.
-app.use(session({ secret: 'anything' }));
-app.use(passport.initialize());
-app.use(passport.session());
-
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
 function loggedIn(req, res, next) {
+	console.log(req.user);
   if (req.user) {
     console.log('yes');
     next();
@@ -74,10 +87,27 @@ app.get('/login/twitter/return', passport.authenticate('twitter', { session: tru
 	res.redirect('/#recipes');
 });
 
-app.get('/checklogin', /*loggedIn,*/ function(req, res) {
-  //console.log(req.user);
+app.get('/checklogin', loggedIn, function(req, res) {
   res.sendStatus(200);
 });
+
+app.get('/logout', function(req, res) {
+	req.session.destroy();
+  res.sendStatus(200);
+});
+
+
+app.get('/login/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/login/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    console.log('Successful authentication, redirect home.');
+    res.redirect('/');
+  });
+
+
 
 
 
