3be2c2c90d6ae6c49e60fddc838741f1cace3ee2
doctafaustus <clowd150@gmail.com>
2017-04-02 15:18:40 -0500

    Remove ingredients from Chrome Extension

--- app.js
+++ app.js
@@ -399,7 +399,6 @@ function addExtensionRecipe(req, res) {
 	var recipe = new Recipe({
 		user_id: req.body.rs_id,
 		recipeName: req.body.recipeName,
-		ingredients: req.body.ingredients,
 		description: req.body.description,
 		url: req.body.url,
 		tags: [],
@@ -408,9 +407,6 @@ function addExtensionRecipe(req, res) {
 		cals: '',
 		favorite: false,
 	});
-	if (req.body.ingredients.length && req.body.ingredients[0] === '') {
-		recipe.ingredients = [];
-	}
   if (req.body.tags) {
     handleTagsAndSave(req.body.rs_id, req.body.tags, recipe, res);
   } else {
